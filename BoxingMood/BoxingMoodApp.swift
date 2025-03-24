3295b57abdd1de5bf49d0759a9101df4e6d86e0e
carlosyrodriguez <carlosrodr07@gmail.com>
2025-03-23 20:53:02 -0400

    Initial Commit

---
+++ BoxingMood/BoxingMoodApp.swift
@@ -0,0 +1,32 @@
+//
+//  BoxingMoodApp.swift
+//  BoxingMood
+//
+//  Created by Los on 3/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BoxingMoodApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
